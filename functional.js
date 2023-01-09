11358872fc1e610653999a0e4890e953e59b682d
yousuf mahomed <118985086+yousufmahomed@users.noreply.github.com>
2023-01-09 18:47:52 +0200

    add reduce functionality

--- functional.js
+++ functional.js
@@ -29,3 +29,46 @@ function twoWordStates(states) {
   return states.filter(state => state.split(/\s+/).length === 2);
 }
 console.log(twoWordStates(states));
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// sum: Imperative solution
+function imperativeSum(elements) {
+  let total = 0;
+  elements.forEach(function(n) {
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum: Functional solution
+function functionalSum(elements) {
+  return elements.reduce((total, n) => { return total += n; });
+}
+console.log(functionalSum(numbers));
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+  let lengths = {};
+  elements.forEach(function(element) {
+    lengths[element] = element.length;
+  });
+  return lengths;
+}
+console.log(imperativeLengths(states));
+
+// lengths: Functional solution
+function functionalLengths(elements) {
+  return elements.reduce((lengths, element) => {
+                          lengths[element] = element.length;
+                          return lengths;
+                        }, {});
+}
+console.log(functionalLengths(states));
+
+// multiplication: Functional solution
+function functionalMultiplication(elements) {
+  return elements.reduce((total, n) => { return total *= n; });
+}
+console.log(functionalMultiplication(numbers));
