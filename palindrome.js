2fe241dfee23d35be1a67bb9229ea5ed0b1bf3dc
yousuf mahomed <118985086+yousufmahomed@users.noreply.github.com>
2023-01-11 20:19:36 +0200

    modify native objects as an exercise

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,6 @@
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
 // Defines a Phrase object.
@@ -14,19 +14,6 @@ function Phrase(content) {
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // Returns translation processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.translation.toLowerCase();
-  }
-}
-
-TranslatedPhrase.prototype = new Phrase();
